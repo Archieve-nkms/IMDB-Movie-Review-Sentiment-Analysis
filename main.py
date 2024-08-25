78792a40547161904eed495457c636db9af33781
NKM <scr08212@gmail.com>
2024-08-25 18:10:08 +0900

    feat: Add GRU_model

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import re #정규식 연산 라이브러리. regex와 비슷한 것 같음
 from keras._tf_keras.keras.datasets import imdb
+from keras._tf_keras.keras.preprocessing.sequence import pad_sequences
+from keras._tf_keras.keras.models import Sequential
+from keras._tf_keras.keras.layers import Dense, GRU, Embedding
+from keras._tf_keras.keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras._tf_keras.keras.models import load_model
+
+
+vocab_size = 100
+max_len = 500
+
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=vocab_size)
+
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+embedding_dim = 100
+hidden_units = 128
+
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim))
+model.add(GRU(hidden_units))
+model.add(Dense(1, activation='sigmoid'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('GRU_model.keras', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=64, validation_split=0.2)
+
+loaded_model = load_model('GRU_model.keras')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
